ce3f6a5be2594082c105c7f1364852b6a6fc81f8
Akash K K <Akash.KK@ibm.com>
2025-01-03 18:40:18 +0530

    feat: add Caffeine caching support and configure cache settings in application

--- user-authentication-service/src/main/java/com/app/auth/user_authentication_service/config/SecurityConfig.java
+++ user-authentication-service/src/main/java/com/app/auth/user_authentication_service/config/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.app.auth.user_authentication_service.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,12 +8,9 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.app.auth.user_authentication_service.service.serviceImpl.UserInfoUserDetailService;
